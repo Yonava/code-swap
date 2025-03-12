d9fc1f93612798c95ac8aef8e3109353f112405b
Viral Rathod <vrathod15@hotmail.com>
2025-03-12 12:56:06 -0400

    Initial test cases component

---
+++ client/src/components/TestCases.tsx
@@ -0,0 +1,16 @@
+export const TestCases = () => {
+  return (
+    <div className="w-full bg-gray-200 text-black p-3">
+      <h2 className="text-lg font-semibold mb-2">Test Cases</h2>
+
+      <div className="flex flex-col gap-2">
+        <div>
+          <p className="font-bold">Input:</p>
+        </div>
+        <div>
+          <p className="font-bold">Expected Output:</p>
+        </div>
+      </div>
+    </div>
+  );
+};
