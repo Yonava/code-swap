9d02ef90422f885bfddcdaab3e8bece054e4cbbf
Wanqi Li <wanli@umass.edu>
2025-03-14 18:05:56 -0400

    Adding code snippet, adjust text spacing

--- client/src/components/ProblemStatement.tsx
+++ client/src/components/ProblemStatement.tsx
@@ -1,38 +1,44 @@
+import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import { Button } from './ui/Button';
-import classes from './tag.module.css'
-
+import classes from './tag.module.css';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export const ProblemStatement = () => {
+  const codeSnippet = `console.log('This is a code snippet example.');
+    `;
+
   const content = `
   <challenge>Challenge Title</challenge>
-  <br>
-  <br>
   <ds> This is the description of the challenge. </ds>
-  <br>
-  <br>
+  
   <sub-title>Example</sub-title>
-  <br>
-  <br>
+  
   <ds>This is description of an example of the challenge. Examples can include <variable>math notations(n=2)</variable> and <ly>code</ly> <lb>code</lb> <lr>code</lr> <lg>code</lg>.</ds>
-  <br>
-  <br>
+  
+  <snip>${codeSnippet}</snip>
+  
   <more>This is an explanation to the description </more>
-  <br>
-  <br>
+  
   <sub-title>Constrain</sub-title>
-  <br>
-  <br>
+  
   <ds>This is the description of this constrain</ds>
   `;
+
   return (
-    <div>
+    <div className={classes.container}>
       <Button>Click me</Button>
-      <ReactMarkdown
-        rehypePlugins={[rehypeRaw]}
-        children = {content + ' ' + classes}
-      />
+      <ReactMarkdown rehypePlugins={[rehypeRaw]} children={content} components={{
+        snip({node, inline, className, children, ...props}) {
+          return (
+            <SyntaxHighlighter style={vscDarkPlus} language="javascript" PreTag="div" {...props}>
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          );
+        }
+      }} />
     </div>
   );
 };
