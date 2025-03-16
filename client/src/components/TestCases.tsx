1d726f2fdd98610c6a718fa336ffc20cdccfae66
yonava <yonavossandreae@gmail.com>
2025-03-16 11:25:44 -0400

    loops

--- client/src/components/TestCases.tsx
+++ client/src/components/TestCases.tsx
@@ -1,30 +1,30 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Button } from './ui/Button';
 
 export const TestCases = () => {
   const [activeTab, setActiveTab] = useState(1);
 
+  const btns = useMemo(() => ({
+    0: 'bt1',
+    1: 'bt2',
+    2: 'bt3'
+  }), [])
+
+  const btnsArr = useMemo(() => {
+    return Object.entries(btns).map(([key, val]) => Number(key))
+  }, [btns])
+
   return (
     <div className="w-full h-full bg-gray-400 text-black p-3">
       <div className="flex gap-2 mb-4">
-        <Button
-          className={`p-2 ${activeTab === 1 ? 'bg-gray-900 text-white' : 'bg-gray-600'}`}
-          onClick={() => setActiveTab(1)}
-        >
-          Case 1
-        </Button>
-        <Button
-          className={`p-2 ${activeTab === 2 ? 'bg-gray-900 text-white' : 'bg-gray-600'}`}
-          onClick={() => setActiveTab(2)}
-        >
-          Case 2
-        </Button>
-        <Button
-          className={`p-2 ${activeTab === 3 ? 'bg-gray-900 text-white' : 'bg-gray-600'}`}
-          onClick={() => setActiveTab(3)}
-        >
-          Case 3
-        </Button>
+        {btnsArr.map((i) => (
+          <Button
+            className={`p-2 ${activeTab === (i+1) ? 'bg-gray-900 text-white' : 'bg-gray-600'}`}
+            onClick={() => setActiveTab(i + 1)}
+          >
+            Case {i+1}
+          </Button>
+        ))}
       </div>
       {activeTab === 1 && (
         <div>
