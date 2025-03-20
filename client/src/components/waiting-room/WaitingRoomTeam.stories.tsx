1cdd1299db80b9c131d9d528a6a019ca04b8fd27
Ashley Bhandari <99090595+ashleybhandari@users.noreply.github.com>
2025-03-19 20:49:12 -0400

    Storybook stories

---
+++ client/src/components/waiting-room/WaitingRoomTeam.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { WaitingRoomTeam } from './WaitingRoomTeam';
+
+const meta = {
+  title: 'UI/WaitingRoomTeam',
+  component: WaitingRoomTeam,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: {
+    defaultName: {
+      description: 'The default name given to the team',
+    },
+  },
+} satisfies Meta<typeof WaitingRoomTeam>;
+
+export default meta;
+
+type Story = StoryObj<typeof WaitingRoomTeam>;
+
+/**
+ * How a team is represented in the Waiting Room. Consists of a configurable name,
+ * avatars and names of each player, and a QR code to join the team.
+ */
+export const Default: Story = {
+  args: {
+    defaultName: 'Team Name',
+  },
+};
