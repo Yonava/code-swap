2655bfd5f7479d855de751f7a050e3561ca66a2f
Viral Rathod <vrathod15@hotmail.com>
2025-03-27 17:21:09 -0400

    Add mock test cases #3

--- client/src/components/test-cases/TestCasesParent.tsx
+++ client/src/components/test-cases/TestCasesParent.tsx
@@ -1,27 +1,17 @@
 import { useState } from 'react';
 import { Button } from '../ui/Button';
 import { TestCase } from './TestCase';
+import { testCases } from '@/mock-data/mock-test-cases';
 
 export const TestCases = () => {
   const [activeTab, setActiveTab] = useState(1);
 
-  const testCases = [
-    { id: 1, input: '[3,3]', output: '6', expected: '6' },
-    { id: 2, input: '[1,2,3]', output: '6', expected: '6' },
-    { id: 3, input: '[2,4,6]', output: '12', expected: '12' },
-    { id: 4, input: '[2,4,6]', output: '13', expected: '12' },
-    { id: 5, input: '[2,4,6]', output: '14', expected: '12' },
-    { id: 6, input: '[2,4,6]', output: '14', expected: '12' },
-    { id: 7, input: '[2,4,6]', output: '14', expected: '12' },
-    { id: 8, input: '[2,4,6]', output: '14', expected: '12' },
-    { id: 9, input: '[2,4,6]', output: '14', expected: '12' },
-    { id: 10, input: '[2,4,6]', output: '14', expected: '12' },
-  ];
+  const localTestCases = testCases;
 
   return (
     <div className="w-full h-full bg-gray-400 text-black p-3">
       <div className="flex gap-2 mb-4">
-        {testCases.map((tc) => (
+        {localTestCases.map((tc) => (
           <Button
             key={tc.id}
             className={`p-2 ${activeTab === tc.id ? 'bg-gray-900 text-white' : 'bg-gray-600'}`}
@@ -31,7 +21,7 @@ export const TestCases = () => {
           </Button>
         ))}
       </div>
-      {testCases.map(
+      {localTestCases.map(
         (tc) =>
           activeTab === tc.id && (
             <TestCase
