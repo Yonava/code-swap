44744828d5bcd28b5127a528b84aa8a45d60b044
Ashley Bhandari <99090595+ashleybhandari@users.noreply.github.com>
2025-03-27 20:15:39 -0400

    Move WaitingRoom to ChallengeView

--- client/src/views/ChallengeView.tsx
+++ client/src/views/ChallengeView.tsx
@@ -1,10 +1,12 @@
 import { CodeEditor } from '@/components/CodeEditor';
 import { ProblemStatement } from '@/components/ProblemStatement';
 import { TitledContainer } from '@/components/TitledContainer';
+import { WaitingRoom } from '@/components/waiting-room/WaitingRoom';
 
 const ChallengeView = () => {
   return (
     <>
+      <WaitingRoom />
       <div className="flex bg-gray-900 w-[100vw] h-[100vh] relative p-2 gap-2">
         <TitledContainer
           title="Challenge Question"
