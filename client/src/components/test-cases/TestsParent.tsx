38943d9064e57058d0274cc43c764d319902f9ba
Viral Rathod <vrathod15@hotmail.com>
2025-03-29 13:27:59 -0400

    Add loading indicator for run button #3

--- client/src/components/test-cases/TestsParent.tsx
+++ client/src/components/test-cases/TestsParent.tsx
@@ -4,20 +4,26 @@ import { ValuesDisplay } from './ValuesDisplay';
 import { TestResultDisplay } from './TestResultDisplay';
 import { TestButtonList } from './TestButtonList';
 import { testCases } from '@/mock-data/mock-test-cases';
+import { Loader2 } from 'lucide-react';
 
 export const TestCases = () => {
   const [activeTab, setActiveTab] = useState(1);
   const [testResult, setTestResult] = useState<number | null>(null);
   const [testsRun, setTestsRun] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const localTestCases = testCases;
 
   const runTests = () => {
-    const passedCount = localTestCases.filter(
-      (tc) => tc.output === tc.expected
-    ).length;
-    setTestResult(passedCount);
-    setTestsRun(true);
+    setLoading(true);
+    setTimeout(() => {
+      const passedCount = localTestCases.filter(
+        (tc) => tc.output === tc.expected
+      ).length;
+      setTestResult(passedCount);
+      setTestsRun(true);
+      setLoading(false);
+    }, 500);
   };
 
   return (
@@ -37,8 +43,15 @@ export const TestCases = () => {
           <Button
             className="bg-green-700 hover:bg-green-600"
             onClick={runTests}
+            disabled={loading}
           >
-            Run
+            {loading ? (
+              <>
+                <Loader2 className="animate-spin mr-2" /> Running..
+              </>
+            ) : (
+              'Run'
+            )}
           </Button>
         </div>
       </div>
