7b258c81890ed6956c37ef07061eca6758e3d3c3
yonava <yonavossandreae@gmail.com>
2025-04-10 23:11:15 -0400

    create match process

--- server/match-making/src/pub-sub/subscribers.ts
+++ server/match-making/src/pub-sub/subscribers.ts
@@ -1,22 +1,27 @@
 import { RedisClient } from "../redis";
 import { MATCH_MAKING_CHANNEL } from "./channels";
 import { CreateMatchRequest, JoinMatchRequest } from '../types';
-import { addPlayerToMatch } from "../matches";
+import { addPlayerToMatch, createNewMatch } from "../matches";
 
 const redisClient = RedisClient.getInstance();
 const { pub: redisPub, sub: redisSub } = redisClient;
 
-redisSub.subscribe(MATCH_MAKING_CHANNEL.SUBSCRIBE.REQUEST_CREATE_MATCH, async (message) => {
+const { PUBLISH, SUBSCRIBE } = MATCH_MAKING_CHANNEL;
+
+redisSub.subscribe(SUBSCRIBE.REQUEST_CREATE_MATCH, async (message) => {
   const data: CreateMatchRequest = JSON.parse(message); // add zod validation
+  const result = await createNewMatch(data.player);
+  const publishedResponse = typeof result === 'string' ? { error: result } : { match: result };
+  redisPub.publish(PUBLISH.RESPONSE_CREATE_MATCH, JSON.stringify(publishedResponse));
 });
 
-redisSub.subscribe(MATCH_MAKING_CHANNEL.SUBSCRIBE.REQUEST_JOIN_MATCH, async (message) => {
+redisSub.subscribe(SUBSCRIBE.REQUEST_JOIN_MATCH, async (message) => {
   const data: JoinMatchRequest = JSON.parse(message); // add zod validation
   const result = await addPlayerToMatch(data);
   const publishedResponse = typeof result === 'string' ? { error: result } : { match: result };
-  redisPub.publish(MATCH_MAKING_CHANNEL.PUBLISH.RESPONSE_JOIN_MATCH, JSON.stringify(publishedResponse));
+  redisPub.publish(PUBLISH.RESPONSE_JOIN_MATCH, JSON.stringify(publishedResponse));
 });
 
-redisSub.subscribe(MATCH_MAKING_CHANNEL.SUBSCRIBE.LEAVE_MATCH, (message) => {
+redisSub.subscribe(SUBSCRIBE.LEAVE_MATCH, (message) => {
   console.log(`Message received: ${message}`);
 });
