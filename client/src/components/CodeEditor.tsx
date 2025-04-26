2d80aa52619d8d06a588a016a65595c27eb79ecc
yonava <yonavossandreae@gmail.com>
2025-04-26 13:29:20 -0400

    editor disable mechanics

--- client/src/components/CodeEditor.tsx
+++ client/src/components/CodeEditor.tsx
@@ -1,13 +1,18 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
 import { useMatchContext } from '@/state/match/useMatchContext';
+import { useMemo } from 'react';
 
 export const CodeEditor = () => {
-  const { challenge } = useMatchContext()
+  const { challenge, newChallengeTime } = useMatchContext()
+
+  const betweenChallenges = useMemo(() => !!newChallengeTime, [newChallengeTime])
+  const editorDisabled = useMemo(() => betweenChallenges || !challenge, [betweenChallenges, challenge])
 
   return (
     <CodeMirror
-      style={{ height: '500px' }}
+      style={{ height: '500px', opacity: editorDisabled ? 0.65 : 1 }}
+      readOnly={editorDisabled}
       height="2000px"
       width="100%"
       theme="dark"
--- server/game-management/dist/pub-sub/subscribers.js
+++ server/game-management/dist/pub-sub/subscribers.js
@@ -36,15 +36,15 @@ const NUMBER_OF_ROUNDS = 1;
  * ie 1 means each player gets 1 stab at the challenge, 2 means player 1 gets 2 stabs at challenge 1
  * while player 2 gets 1 stab at challenge 1 but 2 stabs at challenge 2
  */
-const NUMBER_OF_SWAPS_PER_ROUND = 3;
+const NUMBER_OF_SWAPS_PER_ROUND = 1;
 /**
  * time from start of challenge to end of challenge
  */
-const TIME_FROM_START_TO_END = 10_000;
+const TIME_FROM_START_TO_END = 20_000;
 /**
  * time between the end of a challenge and the start of a new challenge event
  */
-const TIME_FROM_END_TO_START = 3_000;
+const TIME_FROM_END_TO_START = 10_000;
 /**
  * can be used as time from end to end as well
  */
