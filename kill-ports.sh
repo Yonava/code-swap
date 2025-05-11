a78c8571b8e0e4c528daf714279692b531b053fe
yonava <yonavossandreae@gmail.com>
2025-05-11 12:13:12 -0400

    script to clear ports

---
+++ kill-ports.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# locally running microservice ports
+PORTS=(3000 3001 3002 3003 3004)
+
+for PORT in "${PORTS[@]}"; do
+  PID=$(lsof -ti tcp:$PORT)
+  if [ -n "$PID" ]; then
+    echo "Killing process on port $PORT (PID $PID)"
+    kill -9 $PID
+  else
+    echo "No process running on port $PORT"
+  fi
+done
+
+echo "All ports cleared out!"
